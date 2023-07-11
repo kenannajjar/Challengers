07b8fd3fc31644a9f85fb0f4bbc60d22ac0b4428
Kenan Najjar <kenannajjar@berkeley.edu>
2023-07-11 02:59:38 -0700

    UI adjustments

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { SafeAreaView, View, StyleSheet, Text } from 'react-native';
+import { SafeAreaView, View, StyleSheet, Text, ScrollView } from 'react-native';
 import TodaysChallenge from '../components/TodaysChallenge';
 import UserSummary from '../components/UserSummary';
 
 export default function Home() {
     return (
         <SafeAreaView style={styles.container}>
-            <View style = {styles.topBar}>
+            <View style={styles.topBar}>
                 <UserSummary />
             </View>
-            <View style = {{padding: 10}}>
-                <Text style = {styles.text}>Today's Challenge</Text>
-                <TodaysChallenge />
-            </View>
+            <ScrollView>
+
+                <View style={{ padding: 20 }}>
+                    <Text style={styles.text}>Today's Challenge</Text>
+                    <TodaysChallenge />
+                </View>
+            </ScrollView>
+
         </SafeAreaView>
     );
 }
@@ -24,13 +28,14 @@ const styles = StyleSheet.create({
     },
     text: {
         color: 'white',
-        padding: 10,
         fontSize: 27,
-        fontWeight: "bold"
+        fontWeight: "bold",
+        marginBottom: 5
     },
     topBar: {
         flexDirection: "row",
         marginLeft: 10,
-        marginRight: 10
+        marginRight: 10, 
+        marginBottom: 10
     }
 });
